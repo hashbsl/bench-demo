f41a9afebec5c40a2ecf3248fe49bd2ea8871093
jaywantj <jaywantj@birlasoft.com>
2024-01-11 14:00:51 +0530

    dashboard minor changes

--- src/components/dashboard/HomePage.js
+++ src/components/dashboard/HomePage.js
@@ -23,6 +23,7 @@ export default function HomePage() {
           <th scope='col'>Title</th>
           <th scope='col'>Price</th>
           <th scope='col'>Rating</th>
+          <th scope='col'>In Stock</th>
           <th scope='col'>Actions</th>
         </tr>
       </MDBTableHead>
@@ -31,77 +32,32 @@ export default function HomePage() {
           <td>
             <div className='d-flex align-items-center'>
               <div className='ms-3'>
-                <p className='fw-bold mb-1'>{item.id}</p>
-                <p className='text-muted mb-0'>{item.title}</p>
+                <p className='text-muted mb-0'>{item.id}</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div className='d-flex align-items-center'>
+              <div className='ms-3'>
+                <p className='fw-bold mb-1'>{item.title}</p>
               </div>
             </div>
           </td>
           <td>
             <p className='fw-normal mb-1'>{item.price}</p>
-            <p className='text-muted mb-0'>IT department</p>
           </td>
           <td>
             <MDBBadge color='success' pill>
               Active
             </MDBBadge>
           </td>
-          <td>Senior</td>
+          <td>Available</td>
           <td>
             <MDBBtn color='link' rounded size='sm'>
               Edit
             </MDBBtn>
           </td>
         </tr>) : 'No results found'}
-        <tr>
-          <td>
-            <div className='d-flex align-items-center'>
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>Alex Ray</p>
-                <p className='text-muted mb-0'>alex.ray@gmail.com</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>Consultant</p>
-            <p className='text-muted mb-0'>Finance</p>
-          </td>
-          <td>
-            <MDBBadge color='primary' pill>
-              Onboarding
-            </MDBBadge>
-          </td>
-          <td>Junior</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div className='d-flex align-items-center'>
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>Kate Hunington</p>
-                <p className='text-muted mb-0'>kate.hunington@gmail.com</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>Designer</p>
-            <p className='text-muted mb-0'>UI/UX</p>
-          </td>
-          <td>
-            <MDBBadge color='warning' pill>
-              Awaiting
-            </MDBBadge>
-          </td>
-          <td>Senior</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-        </tr>
       </MDBTableBody>
     </MDBTable>
   );
